2c80d8e6a5c869c5f9c99c21a97a0cee11477953
Brownhill Udeh <brownhilludeh@gmail.com>
2023-08-13 21:45:54 +0100

    update

--- index.php
+++ index.php
@@ -1,13 +1,32 @@
+<?php
+$title = 'PHP 101 for all';
+$favwatch = ['neslon', 'gits', 'glitz', 'umarz'];
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>iCode :: <?= $title; ?> </title>
 </head>
 
 <body>
+    <table class="table">
+        <thead>
+            <tr>
+                <th>sn</th>
+                <th>Name</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+                foreach($favwatch as $fw){}
+            ?>
+            
+        </tbody>
+    </table>
     <?php
     echo '<h1>Hello World!</h1>';
 
@@ -32,14 +51,36 @@
     // Array 
     $watch = ['neslon', 'gits', 'glitz', 'umarz'];
 
-    echo "<br>"; 
+    echo "<br>";
 
     print_r($watch);
     // or to get each files
-    echo "<br>"; 
+    echo "<br>";
 
     echo $watch[0];
 
+
+    if (isset($watch[3])) {
+        echo $watch[3];
+    } else {
+        echo 'Watch no de market';
+    }
+
+    //Associate Array
+    $guitar = [
+        'ors' => 'vela',
+        'name' => 'uche prome',
+        'olx' => 'jiji'
+    ];
+    if (isset($guitar['olx'])) {
+        echo $guitar['olx'];
+    } else {
+        echo 'Watch no de market';
+    }
+    // Loopp 
+    // for ($i=0; $i < ; $i++) { 
+    //     # code...
+    // }
     ?>
 </body>
 
